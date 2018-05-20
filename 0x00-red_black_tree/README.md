61ae791ea574299f986122e9a432342596eb6a4d
HoldenGs <holdeng96@gmail.com>
2018-05-20 22:43:42 +0000

    add uncommitted files plus README

---
+++ 0x00-red_black_tree/README.md
@@ -0,0 +1,11 @@
+### Holberton School Datastructure-Algorithms
+
+## Red-Black Tree Implementation
+
+This a C implementation of the commonly used Red-Black Tree. This is binary tree with specific properties that ensure insert, read, and delete times of log(n).
+
+### Compilation and usage
+
+```
+gcc -Werror -Wextra -Wall -pedantic <main_file> <secondary file(s)>
+```
--- 0x00-red_black_tree/rb_trees.h
+++ 0x00-red_black_tree/rb_trees.h
@@ -36,5 +36,12 @@ typedef struct rb_tree_s
 } rb_tree_t;
 
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
+int rb_tree_is_valid(const rb_tree_t *tree);
+rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value);
+void insert_node(rb_tree_t *root, rb_tree_t *node);
+void repair_tree(rb_tree_t *node);
+void rotate_left(rb_tree_t *node);
+void rotate_right(rb_tree_t *node);
+rb_tree_t *sibling(rb_tree_t *node);
 
 #endif /* _RB_TREES_H_ */
