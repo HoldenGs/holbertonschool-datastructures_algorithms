fc2f627eda671d2829f25e8c1352c6e15585e2d4
HoldenGs <holdeng96@gmail.com>
2018-05-28 21:21:07 +0000

    add array to rb tree function

---
+++ 0x00-red_black_tree/3-array_to_rb_tree.c
@@ -0,0 +1,21 @@
+#include "rb_trees.h"
+
+/**
+ * array_to_rb_tree - convert an array to an RB-tree
+ *
+ * @array: array to convert
+ * @size: size of array
+ *
+ * Return: root node of RB-tree
+ */
+rb_tree_t *array_to_rb_tree(int *array, size_t size)
+{
+	size_t i;
+	rb_tree_t *root;
+
+	root = NULL;
+	for (i = 0; i < size; i++)
+		rb_tree_insert(&root, array[i]);
+
+	return (root);
+}
--- 0x00-red_black_tree/rb_trees.h
+++ 0x00-red_black_tree/rb_trees.h
@@ -39,6 +39,7 @@ typedef struct rb_tree_s
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color);
 int rb_tree_is_valid(const rb_tree_t *tree);
 rb_tree_t *rb_tree_insert(rb_tree_t **tree, int value);
+rb_tree_t *array_to_rb_tree(int *array, size_t size);
 
 rb_tree_t *rb_tree_insert_r(rb_tree_t *root, rb_tree_t *node);
 rb_tree_t *correct_red_violation(rb_tree_t *root, int direction);
